a6674b887059d42de068d4946fba534e0d128e65
Francesca Andrian <francescaandrianaaa@gmail.com>
2025-05-05 18:45:39 +0200

    Create a new package and new files in the new package and in templates

---
+++ src/main/java/pixel/academy/thymeleaf_app/controller/DemoController.java
@@ -0,0 +1,19 @@
+package pixel.academy.thymeleaf_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    // aici vom crea mapping-ul pentru "/hello"
+    @GetMapping("/hello")
+    public String sayHello(Model theModel) {
+
+        theModel.addAttribute("theDate", java.time.LocalDateTime.now());
+
+        return "helloworld";  // trebuie sa incarce si sa afiseze o app web; este ca o comanda
+    }
+
+}
